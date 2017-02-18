1f712814f6c56ee51bfa7b75d494b3bcce2efc83
Jonathan Bloedow <jbloedow@intven.com>
2017-02-18 08:00:05 -0800

    Added servo control. Not perfect yet; makes noise in 'off' position, probably need to understand exactly how to use set_PWM_dutycycle and frequency.

--- led_switch.py
+++ led_switch.py
@@ -4,6 +4,9 @@ from cgi import parse_qs, escape
 import pigpio
 
 mypi = pigpio.pi()
+mypi.set_PWM_dutycycle(18, 0)
+mypi.set_PWM_frequency(18, 0)
+
 def application(environ, start_response): 
 	status = '200 OK' 
 	output = '<html>' 
@@ -13,10 +16,12 @@ def application(environ, start_response):
 	if led_val == '' or led_val == "off":
 		# turn off 
 		mypi.write( 17, 0 ) 
+		mypi.set_servo_pulsewidth(18, 500)
 		output += '<a href="' + environ["SCRIPT_NAME"] + '?led=on"><img src="https://thumbs.dreamstime.com/x/toggle-switch-7382047.jpg"/></a><br>' 
 	elif led_val == "on":
 		# turn on 
 		mypi.write( 17, 1 ) 
+		mypi.set_servo_pulsewidth(18, 2500)
 		output += '<a href="' + environ["SCRIPT_NAME"] + '?led=off"><img src="https://thumbs.dreamstime.com/x/tumbler-7633745.jpg"/></a>' 
 	output += '</html>'
 
